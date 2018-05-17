f323d287af63ce0a54cbe9ef99c12f2a16307bc9
Alexander <lss1ash@rambler.ru>
2018-05-17 15:59:23 +0300

    Тестируем вью детализации

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
+import DetailsView from './views/details-view';
+
 const onContentLoaded = () => {
   // console.log(`do smthng!`);
+  const testDetails = new DetailsView();
+  document.querySelector(`body`).appendChild(testDetails.element);
 };
 document.addEventListener(`DOMContentLoaded`, onContentLoaded);
