62f64e599ac444f37d18cb814eefb14d0c5b2ce2
Alexander <lss1ash@rambler.ru>
2018-05-18 01:37:35 +0300

    Выносим вид товара в списке в отдельную вьюшку

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 import DetailsView from './views/details-view';
+import ProductView from './views/product-view';
 
 const onContentLoaded = () => {
-  // console.log(`do smthng!`);
+  const testProduct1 = new ProductView();
+  document.querySelector(`.products-list`).appendChild(testProduct1.element);
+
+  const testProduct2 = new ProductView();
+  document.querySelector(`.products-list`).appendChild(testProduct2.element);
+
+
   const testDetails = new DetailsView();
   document.querySelector(`body`).appendChild(testDetails.element);
 };
